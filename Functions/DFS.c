4ff007ba0ed6d3434a224c24dfbb9f638585d6fa
João Neves <joaopedro.n@outlook.com.br>
2023-04-30 02:09:35 -0300

    corrigindo função DFS

--- Functions/DFS.c
+++ Functions/DFS.c
@@ -9,7 +9,7 @@ void DFS(pGraph G, int v, int *visited, int *discTime) {
     vertex w;
     link list = G->adj[v];
     while(list != NULL) {
-        w = list->next;
+        w = list->w;
         if(!visited[w]) { // se o vértice ainda não foi visitado
             discTime[w] = discTime[v] + 1; // atualiza o tempo de descoberta
             DFS(G, w, visited, discTime); // chama a DFS recursivamente
--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "Functions/FileReader.c"
+#include "Functions/DFS.c"
 
 void main(int argc, char *argv[]){
     if(argc<2) return;
@@ -11,4 +11,16 @@ void main(int argc, char *argv[]){
     //linha 12 e 13 apenas para ver o funcionamento do programa, dps deverão ser excluídas
     printGraph(G); //imprime o grafo
     printf ("O grafo contem %d vertices e %d arestas\n", G->V, G->A); //informação extra
+
+    int numVertices = G->V;
+    int visited[numVertices];
+    int discTime[numVertices];
+    int i;
+    for(i=0; i<numVertices; i++){
+        visited[i] = 0; //marca todos os vértices como não visitados
+        discTime[i] = 0; //inicializa o tempo de descoberta de todos os vértices com 0
+    }
+
+    DFS(G, 0, visited, discTime);
 }
+
